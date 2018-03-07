eebf44d494fb44b76ab130f36d35b36003b26acd
DKhramtsov <d9093211188@gmail.com>
2018-03-07 17:01:38 +0300

    Создан класс Point и всякие функции, которые будт переделаны

--- sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
@@ -2,6 +2,18 @@ package ru.stqa.pft.sandboxCDBP;
 
 public class CalculateDistanceBetweenPoints {
     public static void main(String[] args) {
-
+        hello("world");
+        hello("freedom");
+        hello("atheists");
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+
 }
