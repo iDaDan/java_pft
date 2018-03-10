f86c9f7b1fb56d9c051890d85490bc6dfc27f059
DVHome <d9093211188@gmail.com>
2018-03-10 14:11:57 +0300

    Добавил выисление методом через перегрузку конструкторов

--- sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
@@ -2,17 +2,15 @@ package ru.stqa.pft.sandboxCDBP;
 
 public class CalculateDistanceBetweenPoints {
     public static void main(String[] args) {
-        Point s = new Point();
-        s.a = 3;
-        s.b = 4;
-        Point g = new Point();
-        g.a = 10;
-        g.b = 8;
+        Point s = new Point(3, 4);
+        Point g = new Point(10,8);
+        Point f = new Point (3, 4, 5, 6); //вариант 1 - через другой конструктор
         System.out.println("Расстояние между точками равно " + distance(s, g));
+        System.out.println("Расстояние между точками равно " + f.distance());
     }
 
-    public static double distance (Point s, Point g) {
-        return Math.sqrt((s.a - g.a) * (s.a - g.a) + (s.b - g.b) * (s.b - g.b));
+    public static double distance (Point p1, Point p2) {
+        return Math.sqrt((p1.a - p2.a) * (p1.a - p2.a) + (p1.b - p2.b) * (p1.b - p2.b));
     }
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/Point.java
@@ -3,4 +3,22 @@ package ru.stqa.pft.sandboxCDBP;
 public class Point {
     public double a;
     public double b;
+    public double c;
+    public double d;
+
+    public Point (double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Point (double a, double b, double c, double d) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static double distance () {
+        return Math.sqrt((this.a - this.c) * (this.a - this.c) + (this.b - this.d) * (this.b - this.d));
+    }
+
+
 }
