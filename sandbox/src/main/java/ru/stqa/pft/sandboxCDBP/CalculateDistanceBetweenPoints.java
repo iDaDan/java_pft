90e757fa62038d6fd46b7621bfb60654cc0691a3
DVHome <d9093211188@gmail.com>
2018-03-10 12:12:15 +0300

    Добавил переменные в класс Point, добавил Math.sqrt в мейн класс

--- sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/CalculateDistanceBetweenPoints.java
@@ -5,14 +5,20 @@ public class CalculateDistanceBetweenPoints {
         hello("world");
         hello("freedom");
         hello("atheists");
+        Point s = new Point();
+        s.a = 3;
+        s.b = 4;
+        Point g = new Point();
+        g.a = 10;
+        g.b = 8;
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double l) {
-        return l * l;
+    public static double area(Point s, Point g) {
+        return Math.sqrt((s.a - g.a) *)
     }
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandboxCDBP/Point.java
@@ -1,4 +1,6 @@
 package ru.stqa.pft.sandboxCDBP;
 
 public class Point {
+    public double a;
+    public double b;
 }
