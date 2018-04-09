d960309aae7ffb3683db950b914adbabca3306c9
DKhramtsov <d9093211188@gmail.com>
2018-04-09 20:38:39 +0300

    Добавил проверки, не находимся ли мы на той страницы, с которой можно начать выполнение теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/NavigationHelper.java
@@ -12,11 +12,19 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void goToGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+
+        }
         click(By.linkText("groups"));
-        //System.out.println("1");
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
