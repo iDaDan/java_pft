5c3ce4cf242ce1fc84722b3911ab401845c2829a
acer\admin <d9093211188@gmail.com>
2018-08-24 18:21:54 +0300

    проверка в тесте модификации контакта и группы теперь реализована через лямбда-выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,6 +29,9 @@ public class ContactModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+        before.sort(byId);
+        after.sort(byId);
         assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
