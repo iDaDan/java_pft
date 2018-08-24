7370ca7a906664e65522acacbc95cf2cb9c5fa25
acer\admin <d9093211188@gmail.com>
2018-08-24 18:01:59 +0300

    проверка в тесте создания контакта и группы теперь реализована через лямбда-выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,15 +27,7 @@ public class ContactCreationTest extends TestBase{
             List<ContactData> after = app.getContactHelper().getContactList();
             assertEquals(after.size(), before.size() + 1);
 
-
-            int max = 0;
-            for (ContactData g : after) {
-                if (g.getId() > max) {
-                    max = g.getId();
-                }
-            }
-
-            contact.setId(max);
+            contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
             before.add(contact);
             assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
         }
